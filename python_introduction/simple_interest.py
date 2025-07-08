5e3cee8977750b86837f52659a07cbf445c633c6
David Baiden Amissah <davidbamissah@gmail.com>
2025-07-08 10:00:00 +0000

    Add simple_interest.py for calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# The value of pricipal is in dollars.
+# The rate is an annual percentage.
+# The  number of value represents years.
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print(interest)
+print("The simple interest is", interest)
